efc33b6eb07a0360b99f2f2ad343df631e009bbd
Vitalii Kotelianets <kotelyanets97@gmail.com>
2023-10-17 22:45:33 +0300

    code update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import Form from './components/Form';
+import OpenedTasks from './components/OpenedTasks';
 
 function App() {
+  const [task, setTask] = useState([]);
+
+  const addTask = (newTask) => {
+    setTask([...task, newTask]);
+  };
+
+  const removeItem = (index) => {
+    const updatedTask = task.filter((item, i) => i !== index);
+    setTask(updatedTask);
+  };
+
+  const clearTasks = () => {
+    setTask([]);
+  };
+
   return (
     <div>
-      <Form />
+      <Form addTask={addTask} clearTasks={clearTasks} />
+
+      <OpenedTasks task={task} removeItem={removeItem} />
     </div>
   );
 }
