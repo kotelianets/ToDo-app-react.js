401d4f559b6e67cf20fc811b28f7d9b0be50d2b3
Vitalii Kotelianets <kotelyanets97@gmail.com>
2023-10-17 19:01:34 +0300

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+const Form = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Enter task" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
